3dd1458f9d1105bf0c2f93b4e1936c2c2190e2e8
C3scoo <francescogiorni.2219@libero.it>
2025-04-17 18:30:01 +0200

    Initial commit

---
+++ src/client/java/cesco/nsmod/NsModClient.java
@@ -0,0 +1,10 @@
+package cesco.nsmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class NsModClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
